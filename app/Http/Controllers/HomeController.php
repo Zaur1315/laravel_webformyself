da5becbc0cea265bc26d066b344f0db3d6406f38
Zaur <zaur.quseynzade@gmail.com>
2023-04-11 17:39:43 +0400

    DB connected

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
-//        dump($_ENV['']);
-        return view('home');
+
+//        DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Post 4', 'Lorem Ipsum 4']);
+//        $posts = DB::select("SELECT * FROM posts");
+//        $posts = DB::select("SELECT * FROM posts WHERE id > :id", ['id'=>2]);
+
+        DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL ", [NOW(), NOW()]);
+
+//        dump($posts);
     }
 
     public function test(){
